ead3fb68d52d32b37628f4cbb777b2fba0c0dfaf
toduckhanh <toduc@stat.unipd.it>
2021-03-24 19:15:40 +0100

    R file for lme2

--- R/fit_lme2.R
+++ R/fit_lme2.R
@@ -9,15 +9,13 @@
 #' @import stats
 #' @import utils
 
-reml_loglik_vec <- function(par, D, Y, Z, V, Clus, cls, n_p, n_class){
+reml_loglik_vec <- function(par, D, Y, Z, V, cls, n_p, n_class){
   beta_fit <- par[1:(n_class*n_p)]
   sigma_c <- par[(n_class*n_p + 1)]
   sigma_e <- par[(n_class*n_p + 2):length(par)]
   Sigma <- lapply(1:cls, function(i){
     tem <- tcrossprod(V[[i]])
-    return(sigma_c^2 * tem + diag(as.numeric(matrix(D[Clus == i,], ncol = n_class,
-                                                    byrow = FALSE) %*% sigma_e^2),
-                                  ncol = ncol(tem), nrow = ncol(tem)))
+    return(sigma_c^2 * tem + diag(as.numeric(D[[i]] %*% sigma_e^2), ncol = ncol(tem), nrow = ncol(tem)))
   })
   Sig_chol <- lapply(Sigma, chol)
   Sig_chol_inv <- lapply(Sig_chol, FUN = function(x) backsolve(x, diag(1, ncol(x))))
@@ -33,15 +31,13 @@ reml_loglik_vec <- function(par, D, Y, Z, V, Clus, cls, n_p, n_class){
   return(term_1 + term_2/2)
 }
 
-reml_loglik_vec_sum <- function(par, D, Y, Z, V, Clus, cls, n_p, n_class){
+reml_loglik_vec_sum <- function(par, D, Y, Z, V, cls, n_p, n_class){
   beta_fit <- par[1:(n_class*n_p)]
   sigma_c <- par[(n_class*n_p + 1)]
   sigma_e <- par[(n_class*n_p + 2):length(par)]
   Sigma <- lapply(1:cls, function(i){
     tem <- tcrossprod(V[[i]])
-    return(sigma_c^2 * tem + diag(as.numeric(matrix(D[Clus == i,], ncol = n_class,
-                                                    byrow = FALSE) %*% sigma_e^2),
-                                  ncol = ncol(tem), nrow = ncol(tem)))
+    return(sigma_c^2 * tem + diag(as.numeric(D[[i]] %*% sigma_e^2), ncol = ncol(tem), nrow = ncol(tem)))
   })
   Sig_chol <- lapply(Sigma, chol)
   Sig_chol_inv <- lapply(Sig_chol, FUN = function(x) backsolve(x, diag(1, ncol(x))))
@@ -61,16 +57,14 @@ reml_loglik_vec_sum <- function(par, D, Y, Z, V, Clus, cls, n_p, n_class){
 #'
 #' @description \code{lme2} the cluster-effect models for two-class or three-class setting based on the \code{lme()} routine from \code{nlme}-package.
 #'
-#' @param name.test  ....
-#' @param name.class  ....
-#' @param name.covars  ...
-#' @param name.clust  ...
-#' @param data  ...
-#' @param apVar  a logical value ...
-#' @param residual  a logical value ...
-#' @param fitted a logical value. Default = \code{FALSE}. If set to \code{TRUE}, ...
-#' @param randf  a logical value ...
-#' @param ...  additional arguments for \code{lme()}.
+#' @param name.test  name of variable indicating diagnostic test of biomarker in data. It can be also the transformation, for example, \code{"log(y)"}, where the term \code{log} is for the log-transformation, and \code{y} is the name of test.
+#' @param name.class  name of variable indicating disease classes (diagnostic groups) in data.
+#' @param name.covars  an vector of names of covariates containing in data. The vector can contain also the transformation, for example, \code{c("x1", "sqrt(x2)", "I(x3^2)")}.
+#' @param name.clust  name of variable indicating clusters in data.
+#' @param data  a data frame containing the variables in the model.
+#' @param levl.class  an vector of the unique values (as character strings) that (disease) class might have taken, sorted into increasing order of means of test results corresponding to the disease classes (diagnostic groups). If \code{levl.class = NULL}, the levels will be automatically determined based on data, and sorted into increasing order of means of test results corresponding to the disease classes (diagnostic groups).
+#' @param apVar  a logical value. Default = \code{TRUE}. If set to \code{TRUE}, the covariance matrix for all estimated parameters in model with be obtained by using the sandwich formula.
+#' @param ...  additional arguments for \code{\link{lme}}.
 #'
 #' @details
 #' .....
@@ -84,13 +78,13 @@ reml_loglik_vec_sum <- function(par, D, Y, Z, V, Clus, cls, n_p, n_class){
 #' An object of class "lme2" is a list containing at least the following components:
 #'
 #' \item{call}{the matched call.}
-#' \item{est_para}{the estimate of VUS.}
-#' \item{se_para}{the standard error, obtained by using asymptotic theory or bootstrap resampling method.}
-#' \item{vcov_sand}{the matched call.}
-#' \item{residual}{...}
-#' \item{fitted}{...}
-#' \item{randf}{...}
-#' \item{n_coef}{...}
+#' \item{est_para}{the estimate of all parameters in model.}
+#' \item{se_para}{the standard error, obtained by using the sandwich formula.}
+#' \item{vcov_sand}{the estimated covariance matrix for all estimated parameters, obtained by the sandwich formula.}
+#' \item{residual}{a list of the residuals}
+#' \item{fitted}{a list of the fitted values.}
+#' \item{randf}{a vector of the estimated random effects.}
+#' \item{n_coef}{total numbers of coefficients included in model.}
 #'
 #' @examples
 #' ## Example for two-class setting
@@ -99,6 +93,7 @@ reml_loglik_vec_sum <- function(par, D, Y, Z, V, Clus, cls, n_p, n_class){
 #' out1 <- lme2(name.test = "Y", name.class = "D", name.covars = c("X1", "X2"), name.clust = "id_Clus",
 #'              data = data_2class, apVar = FALSE)
 #' print(out1)
+#' plot(out1)
 #' out2 <- lme2(name.test = "Y", name.class = "D", name.covars = c("X1", "X2"), name.clust = "id_Clus",
 #'              data = data_2class)
 #' print(out2, digits = 3)
@@ -112,10 +107,30 @@ reml_loglik_vec_sum <- function(par, D, Y, Z, V, Clus, cls, n_p, n_class){
 #' out4 <- lme2(name.test = "Y", name.class = "D", name.covars = c("X1", "X2"), name.clust = "id_Clus",
 #'              data = data_3class)
 #' print(out4)
+#' plot(out4)
 #'
 #' @export
-lme2 <- function(name.test, name.class, name.covars, name.clust, data, apVar = TRUE,
-                 residual = FALSE, fitted = FALSE, randf = FALSE, ...){
+lme2 <- function(name.test, name.class, name.covars, name.clust, data, levl.class = NULL, apVar = TRUE, ...){
+  form.mean <- as.formula(paste(name.test, "~", name.class))
+  mean.temp <- aggregate(form.mean, FUN = mean, data = data)
+  temp.levl <- mean.temp[order(mean.temp[,2]), 1]
+  if(is.null(levl.class)){
+    cat("The ordered levels of classes were not assigned by user!\n")
+    cat("The ordered levels of classes are now determined by the orders of averages of diagnostic tests:\n")
+    cat(paste(temp.levl, collapse = " < "), "\n")
+    levl.class <- temp.levl
+  } else{
+    if(all(levl.class == temp.levl)){
+      cat("The orders of inputed levels of classes are the same as the one obtained by the orders of averages of diagnostic tests:\n")
+      cat(paste(levl.class, collapse = " < "),"\n")
+    } else{
+      cat("The orders of inputed levels of classes are not the same as the one obtained by the orders of averages of diagnostic tests:\n")
+      cat("The correct one should be:\n")
+      cat(paste(temp.levl, collapse = " < "),"\n")
+      levl.class <- temp.levl
+    }
+  }
+  data[, name.class] <- factor(data[, name.class], levels = levl.class)
   ## define the formulas for fixed, random and weights
   call <- match.call()
   fixed <- as.formula(paste(name.test, "~", name.class, "+",
@@ -136,40 +151,38 @@ lme2 <- function(name.test, name.class, name.covars, name.clust, data, apVar = T
     id_coef <- c(seq(1, n_coef - 2, by = 3), seq(2, n_coef - 1, by = 3), seq(3, n_coef, by = 3))
   }
   sigma_e_est <- coef(out_lme$modelStruct$varStruct, unconstrained = FALSE, allCoef = TRUE)*out_lme$sigma
-  sigma_e_est <- sigma_e_est[order(names(sigma_e_est))]
+  sigma_e_est <- sigma_e_est[levl.class]
   sigma_c_est <- sqrt(as.numeric(getVarCov(out_lme)))
   par_lme_est <- c(out_lme$coefficients$fixed[id_coef], sigma_c_est, sigma_e_est)
+  icc <- sigma_c_est^2/(sigma_c_est^2 + mean(sigma_e_est)^2)
   fit <- list()
   fit$call <- call
   fit$n_coef <- n_coef
   fit$est_para <- par_lme_est
   names(fit$est_para) <- c(names(par_lme_est)[1:n_coef], "sigma_c", paste0("sigma_", c(1:n_class)))
-  ## preparing input for sandwich covariance matrix
-  Clus <- out_lme$groups[,1]
-  cls <- length(unique(Clus))
-  all.Y <- model.response(model.frame(out_lme$terms, data = data))
-  data_matrix <- model.matrix(out_lme$terms, data = data)
-  D <- data_matrix[, 1:n_class]
-  all.Z <- data_matrix[,id_coef]
-  Y <- split(all.Y, Clus)
-  Z <- lapply(split(as.data.frame(all.Z), Clus), as.matrix)
-  V <- lapply(1:cls, function(x) rep(1, sum(Clus == x)))
-  D_f <- apply(D, 1, function(x) which(x == 1))
-  if(residual){
-    resid <- out_lme$residual[,2]
-    fit$residual <- split(resid, D_f)
-  }
-  if(fitted){
-    fitted <- out_lme$fitted[,2]
-    fit$fitted <- split(fitted, D_f)
-  }
-  if(randf){
-    fit$randf <- ranef(out_lme)$`(Intercept)`
-  }
+  fit$icc <- icc
+  ##
+  resid <- out_lme$residual[,2]
+  fit$residual <- split(resid, data[, name.class])
+  fitted <- out_lme$fitted[,2]
+  fit$fitted <- split(fitted, data[, name.class])
+  fit$randf <- ranef(out_lme)$`(Intercept)`
   if(apVar){
-    jac <- jacobian(reml_loglik_vec, x = par_lme_est, D = D, Y = Y, Z = Z, V = V, Clus = Clus,
+    ## preparing input for sandwich covariance matrix
+    Clus <- out_lme$groups[,1]
+    cls <- length(unique(Clus))
+    n_c <- table(Clus)
+    all.Y <- model.response(model.frame(out_lme$terms, data = data))
+    data_matrix <- model.matrix(out_lme$terms, data = data)
+    all.D <- data_matrix[, 1:n_class]
+    all.Z <- data_matrix[, id_coef]
+    Y <- split(all.Y, Clus)
+    D <- lapply(split(as.data.frame(all.D), Clus), as.matrix)
+    Z <- lapply(split(as.data.frame(all.Z), Clus), as.matrix)
+    V <- lapply(n_c, function(x) rep(1, x))
+    jac <- jacobian(reml_loglik_vec, x = par_lme_est, D = D, Y = Y, Z = Z, V = V,
                     cls = cls, n_p = n_p, n_class = n_class)
-    hes <- hessian(reml_loglik_vec_sum, x = par_lme_est, D = D, Y = Y, Z = Z, V = V, Clus = Clus,
+    hes <- hessian(reml_loglik_vec_sum, x = par_lme_est, D = D, Y = Y, Z = Z, V = V,
                    cls = cls, n_p = n_p, n_class = n_class)
     vcov_sand <- solve(hes) %*% matrix(rowSums(apply(jac, 1, tcrossprod)), n_coef + 1 + n_class,
                                        n_coef + 1 + n_class) %*% solve(hes)
@@ -204,19 +217,62 @@ print.lme2 <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
   if(!is.null(x$se_para)){
     z <- (x$est_para[1:x$n_coef] - rep(0, x$n_coef))/x$se_para[1:x$n_coef]
     p_val <- 2*pnorm(abs(z), lower.tail = FALSE)
-    infer_tab <- cbind(x$est_para, c(x$se_para[1:x$n_coef], rep(NA, length(x$est_para) - x$n_coef)),
-                       c(z, rep(NA, length(x$est_para) - x$n_coef)),
-                       c(p_val, rep(NA, length(x$est_para) - x$n_coef)))
+    infer_tab <- cbind(c(x$est_para, x$icc),
+                       c(x$se_para[1:x$n_coef], rep(NA, length(x$est_para) - x$n_coef + 1)),
+                       c(z, rep(NA, length(x$est_para) - x$n_coef + 1)),
+                       c(p_val, rep(NA, length(x$est_para) - x$n_coef + 1)))
     colnames(infer_tab) <- c("Est.", "Std.Error", "z-value", "p-value")
-    rownames(infer_tab) <- names(x$est_para)
+    rownames(infer_tab) <- c(names(x$est_para), "ICC")
     printCoefmat(infer_tab, has.Pvalue = TRUE, digits = digits, na.print = "--")
   }
   else{
-    infer_tab <- cbind(x$est_para)
+    infer_tab <- cbind(c(x$est_para, x$icc))
     colnames(infer_tab) <- c("Est.")
-    rownames(infer_tab) <- names(x$est_para)
+    rownames(infer_tab) <- c(names(x$est_para), "ICC")
     printCoefmat(infer_tab, has.Pvalue = FALSE, digits = digits)
   }
   invisible(x)
 }
+
+## ---- The function plot.lme2 ----
+#' @title Diagnostic plots for model fitted by lme2
+#'
+#' @description \code{plot.lme2} provides diagnostic plots (based on \code{ggplot()}) for the residuals and cluster effects.
+#'
+#' @method plot lme2
+#' @param x an object of class "lme2", a result of a call to \code{\link{lme2}}.
+#' @param file.name File name to create on disk.
+#'
+#' @details \code{plot.lme2} shows three diagnostic plots: Q-Q plots of residuals, Fitted vs. Residuals, and Q-Q plot of cluster effects.
+#'
+#' @seealso \code{\link{lme2}}
+#'
+#' @import ggplot2
+#' @import ggpubr
+#' @export
+plot.lme2 <- function(x, file.name = NULL){
+  levl <- paste("Class:", names(x$residual))
+  df_fitted_resid <- data.frame(fitted = unlist(x$fitted, use.names = FALSE),
+                                resid = unlist(x$residual, use.names = FALSE),
+                                Groups = factor(rep(levl, sapply(x$residual, length)), levels = levl))
+  p1 <- ggplot(df_fitted_resid, aes(sample = resid)) + stat_qq() + stat_qq_line() +
+    facet_grid( ~ Groups) + xlab("Theoretical Quantiles") + ylab("Sample Quantiles") +
+    labs(subtitle = "Q-Q plots of residuals")
+  p2 <- ggplot(df_fitted_resid, aes(x = fitted, y = resid)) +
+    geom_point() + geom_hline(yintercept = 0, colour = "red", linetype = "dashed") +
+    facet_grid( ~ Groups) + xlab("Fitted") + ylab("Residuals") +
+    labs(subtitle = "Fitted vs. Residuals")
+  p3 <- ggplot(data = data.frame(randf = x$randf, name = rep("Cluster effects", length(x$randf))),
+               aes(sample = randf)) +
+    stat_qq() + stat_qq_line() +
+    facet_grid(~ name) +
+    xlab("Theoretical Quantiles") + ylab("Sample Quantiles") +
+    labs(subtitle = "Q-Q plot of Cluster effects")
+  res <- ggarrange(ggarrange(p1, p2, ncol = 2, nrow = 1), p3, nrow = 2)
+  if(!is.null(file.name)){
+    ggexport(res, filename = file.name)
+  }
+  res
+}
+
 
