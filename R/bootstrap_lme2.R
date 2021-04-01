1c3e9537bfc2ac78f4c7baec254d66f4debcd014
toduckhanh <toduc@stat.unipd.it>
2021-04-01 18:53:57 +0200

    R file for bootstrap for lme2

--- R/bootstrap_lme2.R
+++ R/bootstrap_lme2.R
@@ -4,13 +4,26 @@
 ## Date: 27/03/2021																															##
 ####==========================================================================####
 
+check_order <- function(par, x.val, n_class, n_p){
+  beta_fit <- par[1:(n_class*n_p)]
+  Z <- c(1, x.val)
+  if(n_class == 2){
+    flag <- (beta_fit[1:n_p] - beta_fit[(n_p + 1):(2*n_p)]) %*% Z < 0
+  }
+  if(n_class == 3){
+    flag1 <- (beta_fit[1:n_p] - beta_fit[(n_p + 1):(2*n_p)]) %*% Z < 0
+    flag2 <- (beta_fit[(n_p + 1):(2*n_p)] - beta_fit[(2*n_p + 1):(3*n_p)]) %*% Z < 0
+    flag <- flag1*flag2
+  }
+  return(flag)
+}
 
 #' @import snow
 #' @import doSNOW
 #' @import foreach
-#' @export
-boot_lme2 <- function(name.test, name.class, name.covars, name.clust, data, levl.class = NULL,
-                      B = 250, type = c("cluster", "stratified"), boxcox = TRUE, interval_lambda = c(-2, 2),
+
+boot_lme2 <- function(name.test, name.class, name.covars, name.clust, data, levl.class = NULL, x.val,
+                      B = 250, type = c("cluster", "stratified"), boxcox = TRUE,
                       parallel = FALSE, ncpus = ifelse(parallel, 2, NULL), ...){
   form.mean <- as.formula(paste(name.test, "~", name.class))
   mean.temp <- aggregate(form.mean, FUN = mean, data = data)
@@ -27,7 +40,7 @@ boot_lme2 <- function(name.test, name.class, name.covars, name.clust, data, levl
   type <- match.arg(type)
   # re-sampling process
   bts_func <- function(B, data_list, name.test, name.class, name.covars, name.clust, levl.class, type, boxcox,
-                       interval_lambda){
+                       x.val){
     flag <- 0
     while(flag == 0){
       flag_data <- 0
@@ -49,8 +62,15 @@ boot_lme2 <- function(name.test, name.class, name.covars, name.clust, data, levl
       }
       out <- try(lme2(name.test = name.test, name.class = name.class, name.covars = name.covars,
                       name.clust = name.clust, data = data_bts, levl.class = levl.class,
-                      boxcox = boxcox, apVar = FALSE, trace = FALSE, interval_lambda = interval_lambda), silent = TRUE)
-      if(class(out) != "try-error") flag <- 1
+                      boxcox = boxcox, apVar = FALSE, trace = FALSE),
+                 silent = FALSE)
+      if(class(out) != "try-error") {
+        print(out$est_para)
+        print(length(levl.class))
+        print(out$n_p)
+        print(x.val)
+        flag <- check_order(out$est_para, x.val = x.val, n_class = length(levl.class), n_p = out$n_p)
+      }
     }
     return(out$est_para)
   }
@@ -61,7 +81,7 @@ boot_lme2 <- function(name.test, name.class, name.covars, name.clust, data, levl
     for(i in 1:B){
       out_boot[,i] <- bts_func(B = B, data_list = data_list, name.test = name.test, name.class = name.class,
                                name.covars = name.covars, name.clust = name.clust, levl.class = levl.class,
-                               type = type, boxcox = boxcox, interval_lambda = interval_lambda)
+                               type = type, boxcox = boxcox, x.val = x.val)
     }
   } else{
     cl <- makeCluster(rep("localhost", ncpus), type = "SOCK")
@@ -69,7 +89,7 @@ boot_lme2 <- function(name.test, name.class, name.covars, name.clust, data, levl
     clusterEvalQ(cl, list(library(ClusROC)))
     out_boot <- parSapply(cl, X = 1:B, FUN = bts_func, data_list = data_list, name.test = name.test,
                           name.class = name.class, name.covars = name.covars, name.clust = name.clust,
-                          levl.class = levl.class, type = type, boxcox = boxcox, interval_lambda = interval_lambda)
+                          levl.class = levl.class, type = type, boxcox = boxcox, x.val = x.val)
     stopCluster(cl)
   }
   return(out_boot)
