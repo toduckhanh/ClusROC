bf2e5630ac1529df8f995465576e4c82494eb38c
toduckhanh <toduc@stat.unipd.it>
2022-01-14 14:04:51 +0100

    Generalized Youden Index approach

--- R/thresh_3class_GYI.R
+++ R/thresh_3class_GYI.R
@@ -4,32 +4,32 @@
 ## Date: 19/05/2021																															##
 ####==========================================================================####
 
-GYI_normal <- function(par, par_model, x.val, n_p){
+GYI_normal <- function(par, par_model, z, n_p){
   if(par[1] > par[2]){
     x <- par[1]
     par[1] <- par[2]
     par[2] <- x
   }
-  beta_d <- matrix(par_model[1:(3*n_p)], ncol = n_p, nrow = 3, byrow = TRUE)
+  beta_d <- par_model[1:(3*n_p)]
   sigma_d <- sqrt(par_model[(3*n_p + 2):length(par_model)]^2 + par_model[(3*n_p + 1)]^2)
-  Z <- c(1, x.val)
+  mu_est <- z %*% beta_d
   res_tcfs <- numeric(3)
-  res_tcfs[1] <- pnorm(par[1], mean = Z %*% beta_d[1,], sd = sigma_d[1])
-  res_tcfs[2] <- pnorm(par[2], mean = Z %*% beta_d[2,], sd = sigma_d[2]) -
-    pnorm(par[1], mean = Z %*% beta_d[2,], sd = sigma_d[2])
-  res_tcfs[3] <- 1 - pnorm(par[2], mean = Z %*% beta_d[3,], sd = sigma_d[3])
+  res_tcfs[1] <- pnorm(par[1], mean = mu_est[1], sd = sigma_d[1])
+  res_tcfs[2] <- pnorm(par[2], mean = mu_est[2], sd = sigma_d[2]) -
+    pnorm(par[1], mean = mu_est[2], sd = sigma_d[2])
+  res_tcfs[3] <- 1 - pnorm(par[2], mean = mu_est[3], sd = sigma_d[3])
   return(sum(res_tcfs))
 }
 
-GYI_normal_var <- function(opt_ctps, par_model, vcov_par_model, x.val, n_p){
-  term1 <- grad(GYI_normal, x = par_model, par = opt_ctps, x.val = x.val, n_p = n_p)
-  term2 <- grad(GYI_normal, x = opt_ctps, par_model = par_model, x.val = x.val, n_p = n_p)
-  grad.tcfs_par <- jacobian(TCF_normal, x = par_model, x.val = x.val, thresholds = opt_ctps, n_p = n_p)
-  term3 <- hessian(func = function(par, x.val, n_p){
-    GYI_normal(par = par[1:2], par_model = par[-c(1,2)], x.val = x.val, n_p = n_p)
-  }, x = c(opt_ctps, par_model), x.val = x.val, n_p = n_p)
+GYI_normal_var <- function(opt_ctps, par_model, vcov_par_model, z, n_p){
+  term1 <- grad(GYI_normal, x = par_model, par = opt_ctps, z = z, n_p = n_p)
+  term2 <- grad(GYI_normal, x = opt_ctps, par_model = par_model, z = z, n_p = n_p)
+  grad.tcfs_par <- jacobian(TCF_normal, x = par_model, z = z, thresholds = opt_ctps, n_p = n_p)
+  term3 <- hessian(func = function(par, z, n_p){
+    GYI_normal(par = par[1:2], par_model = par[-c(1,2)], z = z, n_p = n_p)
+  }, x = c(opt_ctps, par_model), z = z, n_p = n_p)
   term3.1 <- term3[1:2, 1:2]
-  term4 <- jacobian(TCF_normal, x = opt_ctps, x.val = x.val, par = par_model, n_p = n_p)
+  term4 <- jacobian(TCF_normal, x = opt_ctps, z = z, par = par_model, n_p = n_p)
   derv.cpt.para <- -solve(term3.1) %*% term3[1:2, -c(1,2)]
   derv.GYI.para <- term1 + term2 %*% derv.cpt.para
   derv.TCF.para <- grad.tcfs_par + term4 %*% derv.cpt.para
@@ -39,10 +39,9 @@ GYI_normal_var <- function(opt_ctps, par_model, vcov_par_model, x.val, n_p){
   return(list(var_J3 = var_J3, vcov_cpts = vcov_cpts, vcov.tcfs = vcov.tcfs))
 }
 
-GYI_est_fun <- function(par_model, x.val, n_p, boxcox, par_boxcox, crit.var = 0.01){
-  beta_d <- matrix(par_model[1:(3*n_p)], ncol = n_p, nrow = 3, byrow = TRUE)
-  Z <- c(1, x.val)
-  mu <- c(Z %*% beta_d[1,], Z %*% beta_d[2,], Z %*% beta_d[3,])
+GYI_est_fun <- function(par_model, z, n_p, boxcox, par_boxcox, crit.var = 0.01){
+  beta_d <- par_model[1:(3*n_p)]
+  mu <- z %*% beta_d
   sigma_d <- sqrt(par_model[(3*n_p + 2):length(par_model)]^2 + par_model[(3*n_p + 1)]^2)
   sigma2_d <- sigma_d^2
   #
