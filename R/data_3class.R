7d44f109a1cf0ca8b1c4a142751729170f3bfdff
toduckhanh <toduc@stat.unipd.it>
2022-01-14 14:02:29 +0100

    data example

---
+++ R/data_3class.R
@@ -0,0 +1,20 @@
+#' @title A simulated data
+#' @name data_3class
+#' @docType data
+#'
+#' @description A simulated data example with 30 clusters.
+#'
+#' @usage data(data_3class)
+#'
+#' @format A data frame with 225 observations from 30 clusters.
+#' \describe{
+#'   \item{\code{id_Clus}}{the id number of cluster.}
+#'   \item{\code{Y}}{test results of subjects.}
+#'   \item{\code{D}}{a factor with 3 levels of disease status, 1, 2, 3. The levels correspond to benign disease, early stage and late stage.}
+#'   \item{\code{X1}}{a continuous covariate}
+#'   \item{\code{X2}}{a binary covariate.}
+#'}
+#'
+#' @keywords data
+#'
+NULL
