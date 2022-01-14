3565a8944cc426a42a9d001f37a0e86c66e16963
toduckhanh <toduc@stat.unipd.it>
2022-01-14 14:05:10 +0100

    Max Volume approach

--- R/thresh_3class_MV.R
+++ R/thresh_3class_MV.R
@@ -4,32 +4,32 @@
 ## Date: 19/05/2021																															##
 ####==========================================================================####
 
-MV_normal <- function(par, par_model, x.val, n_p){
+MV_normal <- function(par, par_model, z, n_p){
   if(par[1] > par[2]){
     x <- par[1]
     par[1] <- par[2]
     par[2] <- x
   }
-  beta_d <- matrix(par_model[1:(3*n_p)], ncol = n_p, nrow = 3, byrow = TRUE)
+  beta_d <- par_model[1:(3*n_p)]
   sigma_d <- sqrt(par_model[(3*n_p + 2):length(par_model)]^2 + par_model[(3*n_p + 1)]^2)
-  Z <- c(1, x.val)
+  mu_est <- z %*% beta_d
   res_tcfs <- numeric(3)
-  res_tcfs[1] <- pnorm(par[1], mean = Z %*% beta_d[1,], sd = sigma_d[1])
-  res_tcfs[2] <- pnorm(par[2], mean = Z %*% beta_d[2,], sd = sigma_d[2]) -
-    pnorm(par[1], mean = Z %*% beta_d[2,], sd = sigma_d[2])
-  res_tcfs[3] <- 1 - pnorm(par[2], mean = Z %*% beta_d[3,], sd = sigma_d[3])
+  res_tcfs[1] <- pnorm(par[1], mean = mu_est[1], sd = sigma_d[1])
+  res_tcfs[2] <- pnorm(par[2], mean = mu_est[2], sd = sigma_d[2]) -
+    pnorm(par[1], mean = mu_est[2], sd = sigma_d[2])
+  res_tcfs[3] <- 1 - pnorm(par[2], mean = mu_est[3], sd = sigma_d[3])
   return(prod(res_tcfs))
 }
 
-MV_normal_var <- function(opt_ctps, par_model, vcov_par_model, x.val, n_p){
-  term1 <- grad(MV_normal, x = par_model, par = opt_ctps, x.val = x.val, n_p = n_p)
-  term2 <- grad(MV_normal, x = opt_ctps, par_model = par_model, x.val = x.val, n_p = n_p)
-  grad.tcfs_par <- jacobian(TCF_normal, x = par_model, x.val = x.val, thresholds = opt_ctps, n_p = n_p)
-  term3 <- hessian(func = function(par, x.val, n_p){
-    MV_normal(par = par[1:2], par_model = par[-c(1,2)], x.val = x.val, n_p = n_p)
-  }, x = c(opt_ctps, par_model), x.val = x.val, n_p = n_p)
+MV_normal_var <- function(opt_ctps, par_model, vcov_par_model, z, n_p){
+  term1 <- grad(MV_normal, x = par_model, par = opt_ctps, z = z, n_p = n_p)
+  term2 <- grad(MV_normal, x = opt_ctps, par_model = par_model, z = z, n_p = n_p)
+  grad.tcfs_par <- jacobian(TCF_normal, x = par_model, z = z, thresholds = opt_ctps, n_p = n_p)
+  term3 <- hessian(func = function(par, z, n_p){
+    MV_normal(par = par[1:2], par_model = par[-c(1,2)], z = z, n_p = n_p)
+  }, x = c(opt_ctps, par_model), z = z, n_p = n_p)
   term3.1 <- term3[1:2, 1:2]
-  term4 <- jacobian(TCF_normal, x = opt_ctps, x.val = x.val, par = par_model, n_p = n_p)
+  term4 <- jacobian(TCF_normal, x = opt_ctps, z = z, par = par_model, n_p = n_p)
   derv.cpt.para <- -solve(term3.1) %*% term3[1:2, -c(1,2)]
   derv.MV.para <- term1 + term2 %*% derv.cpt.para
   derv.TCF.para <- grad.tcfs_par + term4 %*% derv.cpt.para
@@ -40,11 +40,10 @@ MV_normal_var <- function(opt_ctps, par_model, vcov_par_model, x.val, n_p){
 }
 
 
-MV_est_fun <- function(par_model, x.val, n_p, boxcox, par_boxcox, start = NULL, method.optim,
+MV_est_fun <- function(par_model, z, n_p, boxcox, par_boxcox, start = NULL, method.optim,
                        maxit, lower, upper){
-  beta_d <- matrix(par_model[1:(3*n_p)], ncol = n_p, nrow = 3, byrow = TRUE)
-  Z <- c(1, x.val)
-  mu <- c(Z %*% beta_d[1,], Z %*% beta_d[2,], Z %*% beta_d[3,])
+  beta_d <- par_model[1:(3*n_p)]
+  mu <- z %*% beta_d
   sigma_d <- sqrt(par_model[(3*n_p + 2):length(par_model)]^2 + par_model[(3*n_p + 1)]^2)
   sigma2_d <- sigma_d^2
   if(is.null(start)){
@@ -53,7 +52,7 @@ MV_est_fun <- function(par_model, x.val, n_p, boxcox, par_boxcox, start = NULL, 
     start <- c(t1_0, t2_0)
   }
   #
-  MV_optim <- optim(par = start, fn = MV_normal, par_model = par_model, x.val = x.val, n_p = n_p,
+  MV_optim <- optim(par = start, fn = MV_normal, par_model = par_model, z = z, n_p = n_p,
                     method = method.optim, control = list(maxit = maxit, fnscale = -1),
                     lower = lower, upper = upper)
   if(MV_optim$par[1] > MV_optim$par[2]){
