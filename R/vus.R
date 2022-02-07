2ed68adeedf7f2586aae21cbe2e101d27725018c
toduckhanh <toduc@stat.unipd.it>
2022-02-07 18:22:30 +0100

    estimation of cov-specific VUS

--- R/vus.R
+++ R/vus.R
@@ -104,17 +104,13 @@ vus_se <- function(par, vcov_par_model, z, n_p, n_c, n_k, n, p.sss, p.ssk, p.sks
 #' @param out_lme2  an object of class "lme2", a result of a call to \code{\link{lme2}}.
 #' @param x.val  specific value(s) of covariate(s) where the VUS are computed. In case non-covariate, no value is needed to specify. In case of one covariate, \code{x.val} should be a number or a vector. In case of \eqn{p} covariates (\eqn{p > 1}), \code{x.val} should be a vector containing \eqn{p} values of the covariates; or a matrix with \eqn{p} columns and \eqn{m} rows containing values of the covariates if the user wants to compute VUS at \eqn{m} points.
 #' @param apVar  a logical value. If set to \code{TRUE}, the standard error of covariate-specific VUS are computed.
-#' @param ci  a logical value. If TRUE (default), confidence intervals are computed.
-#' @param ci.level  a confidence level to be used for constructing the confidence interval; default is 0.95.
 #' @param subdivisions  the maximum number of subintervals used to approximate the integration. Default is 1000.
 #' @param ...  additional arguments to be passed to \code{\link[stats]{integrate}}
 #'
 #' @details
 #' This function implements estimation method in Xiong et al. (2018) for estimating covariate-specific VUS of a continuous diagnostic test in a clustered design when subjects can be diagnosed in three ordinal groups. The estimator is based on the results of fitting the linear mixed-effect model on the diagnostic tests, which is done by using \code{\link{lme2}} with REML approach. The standard error of the estimated covariate-specific VUS is approximated through the Delta method.
 #'
-#' Before applying the estimation, a quick check for the monotone ordering assumption will be performed. That is, for given values of covariates, three predicted means of three diagnostic groups will be compared. If the assumption does not meet, the covariate-specific VUS at the values of covariates will be not estimated.
-#'
-#' A confidence interval of covariate-specific VUS also is given based on Normal-approximate. If the lower bound (or the upper bound) of the confidence interval is less than 0 (or greater than 1), it will be set as 0 or 1. Furthermore, logit and probit transformations are also applied in order to guarantee the confidence intervals are not outside (0,1). In addition, this function also performs the statistical test, \eqn{H0: VUS = 1/6} versus the alternative of interest.
+#' Before applying the estimation, a quick check for the monotone ordering assumption will be performed. That is, for given values of covariates, three predicted means of three diagnostic groups will be compared. If the assumption does not meet, the covariate-specific VUS at the values of covariates will be not estimated. In addition, this function also performs the statistical test, \eqn{H0: VUS = 1/6} versus the alternative of interest.
 #'
 #'
 #' @return \code{VUS} returns an object of class inheriting from "VUS" class. An object of class "VUS" is a list containing at least the following components:
@@ -122,10 +118,6 @@ vus_se <- function(par, vcov_par_model, z, n_p, n_c, n_k, n, p.sss, p.ssk, p.sks
 #' \item{call}{the matched call.}
 #' \item{vus_est}{a vector containing the estimated covariate-specific VUS.}
 #' \item{vus_se}{a vector containing the standard errors.}
-#' \item{vus_ci_norm}{the normal-approximate confidence interval of covariate-specific VUS.}
-#' \item{vus_ci_log}{the confidence interval of covariate-specific VUS after using logit-transformation.}
-#' \item{vus_ci_prob}{the confidence interval of covariate-specific VUS after using probit-transformation.}
-#' \item{ci.level}{confidence level is used.}
 #' \item{mess_order}{a diagnostic message for monontone ordering of means at given covariates' values.}
 #' \item{x.val}{value(s) of covariate(s).}
 #' \item{n_p}{total numbers of the regressors in the model.}
@@ -146,23 +138,27 @@ vus_se <- function(par, vcov_par_model, z, n_p, n_c, n_k, n, p.sss, p.ssk, p.sks
 #'              data = data_3class)
 #'
 #' ### Estimate covariate-specific VUS at one value of one covariate
-#' VUS(out1, x.val = 0.5, apVar = TRUE, ci = TRUE)
+#' out_vus1 <- VUS(out1, x.val = 0.5, apVar = TRUE)
+#' ci_VUS(out_vus1, ci.level = 0.95)
 #'
 #' ### Estimate covariate-specific VUS at multiple values of one covariate
-#' VUS(out1, x.val = c(-0.5, 0, 0.5), apVar = TRUE, ci = TRUE)
+#' out_vus2 <- VUS(out1, x.val = c(-0.5, 0, 0.5), apVar = TRUE)
+#' ci_VUS(out_vus2, ci.level = 0.95)
 #'
 #' ## Two covariates
 #' out2 <- lme2(fixed.formula = Y ~ X1 + X2, name.class = "D", name.clust = "id_Clus",
 #'              data = data_3class)
 #'
 #' ### Estimate covariate-specific VUS at one point
-#' VUS(out2, x.val = c(1.5, 1), apVar = TRUE, ci = TRUE)
+#' out_vus3 <- VUS(out2, x.val = c(1.5, 1), apVar = TRUE)
+#' ci_VUS(out_vus3, ci.level = 0.95)
 #'
 #' ### Estimate covariate-specific VUS at three points
-#' VUS(out2, x.val = rbind(c(-0.5, 0), c(0.5, 0), c(0.5, 1)), apVar = TRUE, ci = TRUE)
+#' out_vus4 <- VUS(out2, x.val = rbind(c(-0.5, 0), c(0.5, 0), c(0.5, 1)), apVar = TRUE)
+#' ci_VUS(out_vus4, ci.level = 0.95)
 #'
 #' @export
-VUS <- function(out_lme2, x.val, apVar = FALSE, ci = FALSE, ci.level = ifelse(ci, 0.95, NULL),
+VUS <- function(out_lme2, x.val, apVar = FALSE, # ci = FALSE, ci.level = ifelse(ci, 0.95, NULL),
                 subdivisions = 1000, ...){
   ## Check all conditions
   if(isFALSE(inherits(out_lme2, "lme2"))) stop("out_lme2 was not from lme2()!")
@@ -194,7 +190,7 @@ VUS <- function(out_lme2, x.val, apVar = FALSE, ci = FALSE, ci.level = ifelse(ci
     x.val <- matrix(x.val, ncol = n_vb, byrow = FALSE)
   }
   ##
-  if(isFALSE(apVar) & ci) stop("Confidence intervals cannot computed without option of apVar!")
+  # if(isFALSE(apVar) & ci) stop("Confidence intervals cannot computed without option of apVar!")
   if(apVar){
     if(is.null(out_lme2$vcov_sand)) stop("The estimated covariance matrix of parameters was missing!")
     if(any(is.na(out_lme2$vcov_sand))) stop("There are NA values in the estimated covariance matrix of parameters. Unable to estimate standard error.")
@@ -249,22 +245,6 @@ VUS <- function(out_lme2, x.val, apVar = FALSE, ci = FALSE, ci.level = ifelse(ci
       fit$vus_se <- vus_se(par = par, vcov_par_model = vcov_par_model, z = Z, n_p = n_p, n_c = n_c,
                            n_k = n_k, n = n, p.sss = p.sss, p.ssk = p.ssk, p.sks = p.sks, p.skk = p.skk,
                            p.ijk = p.ijk)
-      if(ci){
-        ## Normal-approach with truncated boundary
-        temp <- fit$vus_est + c(-1, 1)*qnorm((1 + ci.level)/2)* fit$vus_se
-        if(temp[1] < 0) temp[1] <- 0
-        if(temp[2] > 1) temp[2] <- 1
-        fit$vus_ci_norm <- matrix(temp, ncol = 2)
-        ## logit-transform
-        logit_vus <- qlogis(fit$vus_est)
-        logit_vus_sd <- fit$vus_se/(fit$vus_est*(1 - fit$vus_est))
-        fit$vus_ci_log <- matrix(plogis(logit_vus + c(-1, 1)*qnorm((1 + ci.level)/2)*logit_vus_sd), ncol = 2)
-        ## probit-transform
-        probit_vus <- qnorm(fit$vus_est)
-        probit_vus_sd <- grad(function(x) qnorm(x), x = fit$vus_est)*fit$vus_se
-        fit$vus_ci_prob <- matrix(pnorm(probit_vus + c(-1, 1)*qnorm((1 + ci.level)/2)*probit_vus_sd), ncol = 2)
-        fit$ci.level <- ci.level
-      }
     }
   }
   if(n_p == 2){ # 1 covariate
@@ -280,26 +260,6 @@ VUS <- function(out_lme2, x.val, apVar = FALSE, ci = FALSE, ci.level = ifelse(ci
         vus_se(par = par, vcov_par_model = vcov_par_model, z = x, n_p = n_p, n_c = n_c, n_k = n_k, n = n,
                p.sss = p.sss, p.ssk = p.ssk, p.sks = p.sks, p.skk = p.skk, p.ijk = p.ijk)
         })
-      if(ci){
-        ## Normal-approach with truncated boundary
-        fit$vus_ci_norm <- t(mapply(FUN = function(x, y) {
-          res <- x + c(-1, 1)*qnorm((1 + ci.level)/2)*y
-          if(res[1] < 0) res[1] <- 0
-          if(res[2] > 1) res[2] <- 1
-          return(res)
-        }, x = fit$vus_est, y = fit$vus_se))
-        ## logit-transform
-        logit_vus <- qlogis(fit$vus_est)
-        logit_vus_sd <- fit$vus_se/(fit$vus_est*(1 - fit$vus_est))
-        fit$vus_ci_log <- t(mapply(FUN = function(x, y) plogis(x + c(-1, 1)*qnorm((1 + ci.level)/2)*y),
-                                   x = logit_vus, y = logit_vus_sd))
-        ## probit-transform
-        probit_vus <- qnorm(fit$vus_est)
-        probit_vus_sd <- grad(function(x) qnorm(x), x = fit$vus_est)*fit$vus_se
-        fit$vus_ci_prob <- t(mapply(FUN = function(x, y) pnorm(x + c(-1, 1)*qnorm((1 + ci.level)/2)*y),
-                                   x = probit_vus, y = probit_vus_sd))
-        fit$ci.level <- ci.level
-      }
     }
   }
   if(n_p > 2){ # multiple covariates
@@ -313,30 +273,95 @@ VUS <- function(out_lme2, x.val, apVar = FALSE, ci = FALSE, ci.level = ifelse(ci
         vus_se(par = par, vcov_par_model = vcov_par_model, z = x, n_p = n_p, n_c = n_c, n_k = n_k, n = n,
                p.sss = p.sss, p.ssk = p.ssk, p.sks = p.sks, p.skk = p.skk, p.ijk = p.ijk)
       })
-      if(ci){
-        ## Normal-approach with truncated boundary,
-        fit$vus_ci_norm <- t(mapply(FUN = function(x, y) {
-          res <- x + c(-1, 1)*qnorm((1 + ci.level)/2)*y
-          if(res[1] < 0) res[1] <- 0
-          if(res[2] > 1) res[2] <- 1
-          return(res)
-          }, x = fit$vus_est, y = fit$vus_se))
-        ## logit-transform
-        logit_vus <- qlogis(fit$vus_est)
-        logit_vus_sd <- fit$vus_se/(fit$vus_est*(1 - fit$vus_est))
-        fit$vus_ci_log <- t(mapply(FUN = function(x, y) plogis(x + c(-1, 1)*qnorm((1 + ci.level)/2)*y),
-                                   x = logit_vus, y = logit_vus_sd))
-        ## probit-transform
-        probit_vus <- qnorm(fit$vus_est)
-        probit_vus_sd <- grad(function(x) qnorm(x), x = fit$vus_est)*fit$vus_se
-        fit$vus_ci_prob <- t(mapply(FUN = function(x, y) pnorm(x + c(-1, 1)*qnorm((1 + ci.level)/2)*y),
-                                    x = probit_vus, y = probit_vus_sd))
-        fit$ci.level <- ci.level
-      }
     }
   }
   fit$n_p <- n_p
   class(fit) <- "VUS"
+  return(fit)
+}
+
+## ---- The function ci_VUS ----
+#' @title Confidence Intervals for Covariate-specific VUS
+#'
+#' @description Computes confidence intervals for covariate-specific VUS obtained by function \code{\link{VUS}}.
+#'
+#' @param x an object of class "VUS", a result of a call to \code{\link{VUS}}.
+#' @param ci.level a confidence level to be used for constructing the confidence interval; default is 0.95.
+#'
+#' @details A confidence interval of covariate-specific VUS also is given based on Normal-approximate. If the lower bound (or the upper bound) of the confidence interval is less than 0 (or greater than 1), it will be set as 0 or 1. Furthermore, logit and probit transformations are also applied in order to guarantee the confidence intervals are not outside (0,1).
+#'
+#' @return \code{ci_VUS} returns an object of class inheriting from "ci_VUS" class. An object of class "ci_VUS" is a list containing at least the following components:
+#'
+#' \item{vus_ci_norm}{the normal-approximate confidence interval of covariate-specific VUS.}
+#' \item{vus_ci_log}{the confidence interval of covariate-specific VUS after using logit-transformation.}
+#' \item{vus_ci_prob}{the confidence interval of covariate-specific VUS after using probit-transformation.}
+#' \item{ci.level}{confidence level is used.}
+#' \item{x.val}{value(s) of covariate(s).}
+#' \item{n_p}{total numbers of the regressors in the model.}
+#'
+#' @seealso \code{\link{VUS}}
+#'
+#' @export
+ci_VUS <- function(x, ci.level = 0.95){
+  if(isFALSE(inherits(x, "VUS"))) stop("The object is not VUS!")
+  if(is.null(x$vus_se)) stop("Can not compute CI without standard error!")
+  n_p <- x$n_p
+  fit <- list()
+  if(n_p == 1){ # no covariate
+    ## Normal-approach with truncated boundary
+    temp <- x$vus_est + c(-1, 1)*qnorm((1 + ci.level)/2)* x$vus_se
+    if(temp[1] < 0) temp[1] <- 0
+    if(temp[2] > 1) temp[2] <- 1
+    fit$vus_ci_norm <- matrix(temp, ncol = 2)
+    ## logit-transform
+    logit_vus <- qlogis(x$vus_est)
+    logit_vus_sd <- x$vus_se/(x$vus_est*(1 - x$vus_est))
+    fit$vus_ci_log <- matrix(plogis(logit_vus + c(-1, 1)*qnorm((1 + ci.level)/2)*logit_vus_sd), ncol = 2)
+    ## probit-transform
+    probit_vus <- qnorm(x$vus_est)
+    probit_vus_sd <- grad(function(x) qnorm(x), x = x$vus_est)*x$vus_se
+    fit$vus_ci_prob <- matrix(pnorm(probit_vus + c(-1, 1)*qnorm((1 + ci.level)/2)*probit_vus_sd), ncol = 2)
+  }
+  if(n_p == 2){ # 1 covariate
+    fit$vus_ci_norm <- t(mapply(FUN = function(x, y) {
+      res <- x + c(-1, 1)*qnorm((1 + ci.level)/2)*y
+      if(res[1] < 0) res[1] <- 0
+      if(res[2] > 1) res[2] <- 1
+      return(res)
+    }, x = x$vus_est, y = x$vus_se))
+    ## logit-transform
+    logit_vus <- qlogis(x$vus_est)
+    logit_vus_sd <- x$vus_se/(x$vus_est*(1 - x$vus_est))
+    fit$vus_ci_log <- t(mapply(FUN = function(x, y) plogis(x + c(-1, 1)*qnorm((1 + ci.level)/2)*y),
+                               x = logit_vus, y = logit_vus_sd))
+    ## probit-transform
+    probit_vus <- qnorm(x$vus_est)
+    probit_vus_sd <- grad(function(x) qnorm(x), x = x$vus_est)*x$vus_se
+    fit$vus_ci_prob <- t(mapply(FUN = function(x, y) pnorm(x + c(-1, 1)*qnorm((1 + ci.level)/2)*y),
+                                x = probit_vus, y = probit_vus_sd))
+  }
+  if(n_p > 2){ # multiple covariates
+    fit$vus_ci_norm <- t(mapply(FUN = function(x, y) {
+      res <- x + c(-1, 1)*qnorm((1 + ci.level)/2)*y
+      if(res[1] < 0) res[1] <- 0
+      if(res[2] > 1) res[2] <- 1
+      return(res)
+    }, x = x$vus_est, y = x$vus_se))
+    ## logit-transform
+    logit_vus <- qlogis(x$vus_est)
+    logit_vus_sd <- x$vus_se/(x$vus_est*(1 - x$vus_est))
+    fit$vus_ci_log <- t(mapply(FUN = function(x, y) plogis(x + c(-1, 1)*qnorm((1 + ci.level)/2)*y),
+                               x = logit_vus, y = logit_vus_sd))
+    ## probit-transform
+    probit_vus <- qnorm(x$vus_est)
+    probit_vus_sd <- grad(function(x) qnorm(x), x = x$vus_est)*x$vus_se
+    fit$vus_ci_prob <- t(mapply(FUN = function(x, y) pnorm(x + c(-1, 1)*qnorm((1 + ci.level)/2)*y),
+                                x = probit_vus, y = probit_vus_sd))
+  }
+  fit$ci.level <- ci.level
+  fit$n_p <- n_p
+  fit$x.val <- x$x.val
+  class(fit) <- "ci_VUS"
   return(fit)
 }
 
@@ -396,22 +421,6 @@ print.VUS <- function(x, digits = 3, call = TRUE, ...){
     cat("z-value and p-value are for testing the null hypothesis H0: VUS = 1/6 \n")
     # rownames(infer_tab) <- labels
     # printCoefmat(infer_tab, has.Pvalue = TRUE, digits = digits, na.print = "--") # cs.ind = 2:3, tst.ind = 4,
-    if(!is.null(x$vus_ci_norm)){
-      ci.tab <- cbind(x$vus_ci_norm, x$vus_ci_log, x$vus_ci_prob)
-      ci.tab <- format(round(ci.tab, digits = digits))
-      res.ci.tab <- data.frame(labels,
-                               apply(matrix(ci.tab[,1:2], ncol = 2, byrow = FALSE), 1,
-                                     function(y) paste0("(", paste(y, collapse = ", "), ")")),
-                               apply(matrix(ci.tab[,3:4], ncol = 2, byrow = FALSE), 1,
-                                     function(y) paste0("(", paste(y, collapse = ", "), ")")),
-                               apply(matrix(ci.tab[,5:6], ncol = 2, byrow = FALSE), 1,
-                                     function(y) paste0("(", paste(y, collapse = ", "), ")")))
-      colnames(res.ci.tab) <- c("Covariates Values", "Normal approximation", "Logit transformation",
-                                "Probit transformation")
-      cat("---\n")
-      cat(paste0("The ", x$ci.level*100, "% confidence Intervals for covariate-specific VUS:\n"))
-      print(res.ci.tab, quote = FALSE, right = TRUE, na.print = "--", row.names = FALSE, print.gap = 3)
-    }
   }
   else{
     infer_tab <- data.frame(labels, x$vus_est)
@@ -421,6 +430,49 @@ print.VUS <- function(x, digits = 3, call = TRUE, ...){
     cat("Covariate-specific VUS: \n")
     print(infer_tab, quote = FALSE, right = TRUE, na.print = "--", row.names = FALSE, ...)
   }
+  cat("\n")
+  invisible(x)
+}
+
+## ---- The function print.ci_VUS ----
+#' @title Print summary results of ci_VUS
+#'
+#' @description \code{print.ci_VUS} prints the results for the output of function \code{\link{ci_VUS}}.
+#'
+#' @method print ci_VUS
+#' @param x an object of class "ci_VUS", a result of a call to \code{\link{ci_VUS}}.
+#' @param digits minimal number of significant digits, see \code{\link{print.default}}.
+#' @param ... further arguments passed to \code{\link{print}} method.
+#'
+#' @details \code{print.ci_VUS} shows a nice format of the summary table for CI for covariate-specific VUS estimates.
+#'
+#' @seealso \code{\link{VUS}}
+#'
+#' @export
+print.ci_VUS <- function(x, digits = 3, ...){
+  if(isFALSE(inherits(x, "ci_VUS"))) stop("The object is not ci_VUS!")
+  if(x$n_p == 1){
+    labels <- "Intercept"
+  }
+  if(x$n_p == 2) {
+    labels <- as.character(x$x.val)
+  }
+  if(x$n_p > 2) {
+    labels <- apply(x$x.val, 1, function(y) paste0("(", paste(y, collapse = ", "), ")"))
+  }
+  ci.tab <- cbind(x$vus_ci_norm, x$vus_ci_log, x$vus_ci_prob)
+  ci.tab <- format(round(ci.tab, digits = digits))
+  res.ci.tab <- data.frame(labels,
+                           apply(matrix(ci.tab[,1:2], ncol = 2, byrow = FALSE), 1,
+                                 function(y) paste0("(", paste(y, collapse = ", "), ")")),
+                           apply(matrix(ci.tab[,3:4], ncol = 2, byrow = FALSE), 1,
+                                 function(y) paste0("(", paste(y, collapse = ", "), ")")),
+                           apply(matrix(ci.tab[,5:6], ncol = 2, byrow = FALSE), 1,
+                                 function(y) paste0("(", paste(y, collapse = ", "), ")")))
+  colnames(res.ci.tab) <- c("Covariates Values", "Normal approximation", "Logit transformation",
+                            "Probit transformation")
+  cat(paste0("The ", x$ci.level*100, "% confidence intervals for covariate-specific VUS:\n"))
+  print(res.ci.tab, quote = FALSE, right = TRUE, na.print = "--", row.names = FALSE, print.gap = 3)
   cat("\n")
   invisible(x)
 }
