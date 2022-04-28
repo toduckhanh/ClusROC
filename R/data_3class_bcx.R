17b644551777dd5717aef7b86487ae4d3da32c43
toduckhanh <toduc@stat.unipd.it>
2022-04-28 16:38:56 +0200

    data example Box-Cox

---
+++ R/data_3class_bcx.R
@@ -0,0 +1,19 @@
+#' @title A simulated data
+#' @name data_3class_bcx
+#' @docType data
+#'
+#' @description A simulated data example with 60 clusters. This data is used in an example of an analysis with the Box-Cox transformation.
+#'
+#' @usage data(data_3class_bcx)
+#'
+#' @format A data frame with 582 observations from 60 clusters.
+#' \describe{
+#'   \item{\code{id_Clus}}{the id number of cluster.}
+#'   \item{\code{Y}}{test results of subjects.}
+#'   \item{\code{D}}{a factor with 3 levels of disease status, 1, 2, 3. The levels correspond to benign disease, early stage and late stage.}
+#'   \item{\code{X}}{a continuous covariate.}
+#'}
+#'
+#' @keywords data
+#'
+NULL
