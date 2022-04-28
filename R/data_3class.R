a4aecd5e0ddf9c59b5de45e7bff1e31761e7d7f9
toduckhanh <toduc@stat.unipd.it>
2022-04-28 16:38:43 +0200

    data example

--- R/data_3class.R
+++ R/data_3class.R
@@ -11,7 +11,7 @@
 #'   \item{\code{id_Clus}}{the id number of cluster.}
 #'   \item{\code{Y}}{test results of subjects.}
 #'   \item{\code{D}}{a factor with 3 levels of disease status, 1, 2, 3. The levels correspond to benign disease, early stage and late stage.}
-#'   \item{\code{X1}}{a continuous covariate}
+#'   \item{\code{X1}}{a continuous covariate.}
 #'   \item{\code{X2}}{a binary covariate.}
 #'}
 #'
