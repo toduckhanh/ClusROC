479d41a9eac0c6593f866c04a240542112bd8ed0
toduckhanh <toduc@stat.unipd.it>
2022-05-12 10:32:16 +0200

    help page of MouseNeurons data

--- R/data_MouseNeurons.R
+++ R/data_MouseNeurons.R
@@ -2,7 +2,7 @@
 #' @name MouseNeurons
 #' @docType data
 #'
-#' @description A subset of mouse brain cells data used in To el al. (2022).
+#' @description A subset of mouse brain cells data used in To el al. (2022). This is used to evaluate the ability of Lamp5 gene to discriminate three types of glutamatergic neurons. A full data is publicly available at \url{https://portal.brain-map.org/atlases-and-data/rnaseq/mouse-v1-and-alm-smart-seq}
 #'
 #' @usage data(MouseNeurons)
 #'
@@ -13,8 +13,8 @@
 #'   \item{\code{genotype_id}}{the mouse genotype (which yield 23 clusters).}
 #'   \item{\code{sex}}{the gender of mouse.}
 #'   \item{\code{age_days}}{the age of mouse, in days.}
-#'   \item{\code{Slc17a7_cpm}}{count per million of Solute Carrier Family 17 Member 7 (Slc17a7) gene expression.}
-#'   \item{\code{Lamp5_cpm}}{count per million of Lysosomal Associated Membrane Protein Family Member 5 (Lamp5) gene expression.}
+#'   \item{\code{Slc17a7_cpm}}{count per million of Slc17a7 (Solute Carrier Family 17 Member 7) gene expression.}
+#'   \item{\code{Lamp5_cpm}}{count per million of Lamp5 (Lysosomal Associated Membrane Protein Family Member 5) gene expression.}
 #'}
 #'
 #' @keywords data
