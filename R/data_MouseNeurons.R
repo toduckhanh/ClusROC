a4cdb27c0040fd92ff36d34895034be9c34b91ca
toduckhanh <toduc@stat.unipd.it>
2022-05-12 10:09:13 +0200

    file page for MouseNeuron data

---
+++ R/data_MouseNeurons.R
@@ -0,0 +1,22 @@
+#' @title A subset of mouse brain cells data
+#' @name MouseNeurons
+#' @docType data
+#'
+#' @description A subset of mouse brain cells data used in To el al. (2022).
+#'
+#' @usage data(MouseNeurons)
+#'
+#' @format A data frame with 860 observations from 23 clusters and 7 variables:
+#' \describe{
+#'   \item{\code{sample_name}}{name of each observation.}
+#'   \item{\code{subclass_label}}{a factor with 3 levels (types) of glutamatergic neurons, i.e., L2/3 IT (Layer 2/3 Intratelencephalic), L4 (Layer 4) and L5 PT (Layer 5 Pyramidal Tract) neurons.}
+#'   \item{\code{genotype_id}}{the mouse genotype (which yield 23 clusters).}
+#'   \item{\code{sex}}{the gender of mouse.}
+#'   \item{\code{age_days}}{the age of mouse, in days.}
+#'   \item{\code{Slc17a7_cpm}}{count per million of Solute Carrier Family 17 Member 7 (Slc17a7) gene expression.}
+#'   \item{\code{Lamp5_cpm}}{count per million of Lysosomal Associated Membrane Protein Family Member 5 (Lamp5) gene expression.}
+#'}
+#'
+#' @keywords data
+#'
+NULL
