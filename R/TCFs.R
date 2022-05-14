0bf4aca5c6bb6ac9336358e57f3e8332d5b7ad32
toduckhanh <toduc@stat.unipd.it>
2022-05-14 15:41:01 +0200

    True Class Fractions

--- R/TCFs.R
+++ R/TCFs.R
@@ -1,9 +1,8 @@
 ####==========================================================================####
 ## This file consists of functions for estimating TCFs at a fixed               ##
 ## pair of thresholds                                                           ##
-## Date: 21/10/2021																															##
+## Date: 12/05/2022																															##
 ####==========================================================================####
-
 
 TCF_normal <- function(par, z, thresholds, n_p, boxcox = FALSE){
   beta_d <- par[1:(3*n_p)]
@@ -56,7 +55,7 @@ TCF_normal_vcov <- function(par_model, z, thresholds, vcov_par_model, n_p, fixed
 #' @description \code{TCFs} estimates covariate-specific True Class Fractions (TCFs), at a specified pair of thresholds, of a continuous diagnostic test in a clustered design with three ordinal groups. This function allows to estimate covariate-specific TCFs at multiple points for covariates.
 #'
 #' @param out_lme2  an object of class "lme2", a result of \code{\link{lme2}} call.
-#' @param x.val  specific value(s) of covariate(s) where the TCFs are estimated. In absence of covariate, no values have to be specified. In case of one covariate, \code{x.val} should be a number. In case of \eqn{p} covariates (\eqn{p > 1}), \code{x.val} should be a vector containing \eqn{p} values; or a matrix with \eqn{p} columns and \eqn{m} rows containing values of the covariates if the user wants to estimate at \eqn{m} points.
+#' @param newdata  a data frame (containing specific value(s) of covariate(s)) in which to look for variables with which to estimate covariate-specific TCFs. In absence of covariate, no values have to be specified.
 #' @param thresholds  specified pair of thresholds.
 #' @param apVar  logical value. If set to \code{TRUE}, the variance-covariance matrix of estimated covariate-specific TCFs is estimated.
 #' @details
@@ -71,7 +70,7 @@ TCF_normal_vcov <- function(par_model, z, thresholds, vcov_par_model, n_p, fixed
 #' \item{tcf_vcov}{a matrix or list of matrices containing the estimated variance-covariance matrices.}
 #' \item{thresholds}{specified pair of thresholds.}
 #' \item{mess_order}{a diagnostic message from checking the monontone ordering.}
-#' \item{x.val}{value(s) of covariate(s).}
+#' \item{newdata}{value(s) of covariate(s).}
 #' \item{n_p}{total number of regressors in the model.}
 #'
 #' Generic functions such as \code{print} is also used to show the results.
@@ -88,7 +87,8 @@ TCF_normal_vcov <- function(par_model, z, thresholds, vcov_par_model, n_p, fixed
 #'              data = data_3class)
 #'
 #' ### Estimate TCFs at one single value of X1, (t1, t2) = (1, 4)
-#' out_tcfs_1 <- TCFs(out_lme2 = out1, x.val = 1, thresholds = c(1, 4), apVar = TRUE)
+#' out_tcfs_1 <- TCFs(out_lme2 = out1, newdata = data.frame(X1 = 1),
+#'                    thresholds = c(1, 4), apVar = TRUE)
 #' print(out_tcfs_1)
 #'
 #' ## Two covariates
@@ -96,48 +96,35 @@ TCF_normal_vcov <- function(par_model, z, thresholds, vcov_par_model, n_p, fixed
 #'              data = data_3class)
 #'
 #' ### Estimate covariate-specific TCFs at point (X1, X2) = (1, 0), and (t1, t2) = (1, 4)
-#' out_tcfs_2 <- TCFs(out_lme2 = out2, x.val = c(1, 0), thresholds = c(1, 4), apVar = TRUE)
+#' out_tcfs_2 <- TCFs(out_lme2 = out2, newdata = data.frame(X1 = 1, X2 = 0),
+#'                    thresholds = c(1, 4), apVar = TRUE)
 #' print(out_tcfs_2)
 #'
 #' ### Estimate covariate-specific TCFs at three points and (t1, t2) = (1, 4)
-#' out_tcfs_3 <- TCFs(out_lme2 = out2, x.val = rbind(c(-0.5, 0), c(0.5, 0), c(0.5, 1)),
+#' out_tcfs_3 <- TCFs(out_lme2 = out2,
+#'                    newdata = data.frame(X1 = c(-0.5, 0.5, 0.5), X2 = c(0, 0, 1)),
 #'                    thresholds = c(1, 4), apVar = TRUE)
 #' print(out_tcfs_3)
 #'
 #' @export
-TCFs <- function(out_lme2, x.val, thresholds, apVar = FALSE){
+TCFs <- function(out_lme2, newdata, thresholds, apVar = FALSE){
   if(isFALSE(inherits(out_lme2, "lme2"))) stop("out_lme2 was not from lme2()!")
   n_p <- out_lme2$n_p
   if(out_lme2$n_coef/n_p != 3) stop("There is not a case of three-class setting!")
   if(n_p == 1){
-    if(!missing(x.val)) {
-      if(!is.null(x.val)) warning("Sepecified value(s) of covariate(s) are not used!", call. = FALSE)
+    if(!missing(newdata)) {
+      if(!is.null(newdata)) warning("Sepecified value(s) of covariate(s) are not used!", call. = FALSE)
     }
-    x.val <- NULL
-  }
-  if(n_p == 2){
-    if(missing(x.val)) stop("Please input specific value(s) of covariate.")
-    if(is.null(x.val)) stop("Please input specific value(s) of covariate.")
-    if(!inherits(x.val, "numeric")) stop("For the case of 1 covariate, please input a number or a vector.")
-    if(any(is.na(x.val))) stop("NA value(s) are not allowed!")
-  }
-  if(n_p > 2){
-    if(missing(x.val)) stop("Please input specific value(s) of covariates.")
-    if(is.null(x.val)) stop("Please input specific value(s) of covariates.")
-    n_vb <- attr(out_lme2$terms, "n_vb")
-    if(inherits(x.val, "numeric")){
-      if(length(x.val) != n_vb) stop(paste("For case of", n_vb, "covariates, please input a vector of", n_vb, "values of covariates."))
-    }
-    if(inherits(x.val, "matrix")) {
-      if(ncol(x.val) != n_vb) stop(paste("For case of m points of", n_vb, "covariates, please input a matrix with", n_vb, "columns and m rows containing values of covariates."))
-    }
-    if(any(is.na(x.val))) stop("NA value(s) not allowed!")
-    x.val <- matrix(x.val, ncol = n_vb, byrow = FALSE)
+    newdata <- NULL
+  } else {
+    if(missing(newdata)) stop("Please input a data frame including specific value(s) of covariate(s).")
+    if(is.null(newdata)) stop("Please input a data frame including specific value(s) of covariate(s).")
+    if(!inherits(newdata, "data.frame")) stop("Please input a data frame including specific value(s) of covariate(s).")
+    if(any(is.na(newdata))) stop("NA value(s) are not allowed!")
   }
   if(apVar){
     if(is.null(out_lme2$vcov_sand)) stop("The estimated covariance matrix of parameters was missing!")
     if(any(is.na(out_lme2$vcov_sand))) stop("There are NA values in the estimated covariance matrix of parameters. Unable to estimate standard error of TCFs.")
-    # vcov_par_model <- out_lme2$vcov_sand[1:(out_lme2$n_coef + 4), 1:(out_lme2$n_coef + 4)]
   }
   call <- match.call()
   fit <- list()
@@ -145,7 +132,7 @@ TCFs <- function(out_lme2, x.val, thresholds, apVar = FALSE){
   ## main
   par_model <- out_lme2$est_para
   ## Check the ordering of means: mu_1 < mu_2 < mu_3
-  Z <- make_data(out_lme2, x.val, n_p)
+  Z <- make_data(out_lme2, newdata, n_p)
   res_check <- check_mu_order(Z, par_model, n_p)
   if(all(res_check$status == 0))
     stop("The assumption of montone ordering DOES NOT hold for all the value(s) of the covariate(s)")
@@ -156,34 +143,19 @@ TCFs <- function(out_lme2, x.val, thresholds, apVar = FALSE){
     message(mess_order)
   }
   Z <- res_check$Z_new
-  if(n_p == 1){ # no covariate
-    fit$x.val <- x.val
-    fit$tcfs_est <- TCF_normal(par = par_model, z = Z, thresholds = thresholds, n_p = n_p,
+  if(n_p == 1){ # with out covariate
+    fit$newdata <- newdata
+    fit$tcfs_est <- TCF_normal(par = par_model, z = Z[[1]], thresholds = thresholds, n_p = n_p,
                                boxcox = out_lme2$boxcox)
     if(apVar){
       fit$tcfs_cov <- list()
-      fit$tcfs_cov[[1]] <- TCF_normal_vcov(par_model = par_model, z = Z, thresholds = thresholds,
+      fit$tcfs_cov[[1]] <- TCF_normal_vcov(par_model = par_model, z = Z[[1]], thresholds = thresholds,
                                            vcov_par_model = out_lme2$vcov_sand, n_p = n_p, fixed = TRUE,
                                            boxcox = out_lme2$boxcox)
     }
-  }
-  if(n_p == 2){ # 1 covariate
-    fit$x.val <- x.val[res_check$status != 0]
-    # tcfs_vector <- Vectorize(TCF_normal, vectorize.args = "x.val")
-    fit$tcfs_est <- sapply(Z, function(x){
-      TCF_normal(par = par_model, z = x, thresholds = thresholds, n_p = n_p, boxcox = out_lme2$boxcox)
-    })
-    if(apVar){
-      # tcfs_se_vector <- Vectorize(TCF_normal_vcov, vectorize.args = "x.val", SIMPLIFY = FALSE)
-      fit$tcfs_cov <- lapply(Z, function(x){
-        TCF_normal_vcov(par_model = par_model, z = x, thresholds = thresholds,
-                        vcov_par_model = out_lme2$vcov_sand, n_p = n_p, fixed = TRUE,
-                        boxcox = out_lme2$boxcox)
-      })
-    }
-  }
-  if(n_p > 2){ # multiple covariates
-    fit$x.val <- matrix(x.val[res_check$status != 0,], ncol = n_vb, byrow = FALSE)
+  } else { # with covariate
+    fit$newdata <- as.data.frame(newdata[res_check$status != 0,])
+    names(fit$newdata) <- names(newdata)
     fit$tcfs_est <- sapply(Z, function(x){
       TCF_normal(par = par_model, z = x, thresholds = thresholds, n_p = n_p, boxcox = out_lme2$boxcox)
     })
@@ -231,10 +203,10 @@ print.TCFs <- function(x, digits = 3, call = TRUE, ...){
     labels <- "Intercept"
   }
   if(x$n_p == 2) {
-    labels <- as.character(x$x.val)
+    labels <- apply(x$newdata, 1, function(y) paste0(y))
   }
   if(x$n_p > 2) {
-    labels <- apply(x$x.val, 1, function(y) paste0("(", paste(y, collapse = ", "), ")"))
+    labels <- apply(x$newdata, 1, function(y) paste0("(", paste(y, collapse = ", "), ")"))
   }
   infer_tab <- data.frame(labels, x$tcfs_est[,1], x$tcfs_est[,2], x$tcfs_est[,3])
   infer_tab[,2:4] <- signif(infer_tab[,2:4], digits = digits)
